b284f350a588712edfb8de79745b86fd3f38840a
leandro soto <leandroesoto@gmail.com>
2022-07-11 21:58:37 -0300

    actualizando

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          esta es mi aplicación con react.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/leandroesoto/shopp-leandroSoto"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ir al repo en Github
         </a>
       </header>
     </div>
