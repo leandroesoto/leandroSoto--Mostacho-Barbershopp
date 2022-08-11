1b6e848f26e62ee959efeb0fdabf794e5e73221b
leandroesoto <leandroesoto@gmail.com>
2022-08-10 23:01:28 -0300

    corrigiendo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import NavBar from './components/NavBar';
 import ContentWrapper from './components/ContentWrapper';
 import CartProvider from './context/CartContext';
+import { doc, getDocs, getFirestore,  } from 'firebase/firestore';
+
+
+
+
 
 function App() {
  
--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcgIL4hkq4jPgHhOsBmOTXV3EO6N3QcVI",
+  authDomain: "mi-proyecto-282119.firebaseapp.com",
+  projectId: "mi-proyecto-282119",
+  storageBucket: "mi-proyecto-282119.appspot.com",
+  messagingSenderId: "295004043369",
+  appId: "1:295004043369:web:548e8d71db8b5f2cec8af8"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
